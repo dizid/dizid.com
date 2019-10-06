c1a052865392aef5ffde4478f3924fc5655d3f92
dizid <marc@dizid.com>
2019-10-06 18:34:42 +0200

    Commit to Netlify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './router'
+import VueAnalytics from 'vue-analytics'
+import 'bulma/css/bulma.css'
+import Vuelidate from "vuelidate"
+Vue.use(Vuelidate)
 Vue.config.productionTip = false
 
+Vue.use(VueAnalytics, {
+  id: 'UA-285785-14',
+  router
+})
+
 new Vue({
-  render: function (h) { return h(App) },
+  router,
+  render: h => h(App)
 }).$mount('#app')
